da0a1babe3dd548b4e15d90183f0bed72c0be7ac
simone <simon387@hotmail.it>
2022-01-14 03:53:33 +0100

    added_footer

--- components/footer.php
+++ components/footer.php
@@ -1,43 +1,36 @@
-<!-- footer section -->
+<?php
+
+$f = fopen("changelog.txt", 'r');
+$version = fgets($f);
+fclose($f);
+?>
 <footer class="footer_section">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 footer-col">
 				<div class="footer_contact">
-					<h4>
-						Contact Us
-					</h4>
+					<h4>Contact Us</h4>
 					<div class="contact_link_box">
 						<a href="">
 							<i class="fa fa-map-marker" aria-hidden="true"></i>
-							<span>
-                  Location
-                </span>
+							<span>Location</span>
 						</a>
 						<a href="">
 							<i class="fa fa-phone" aria-hidden="true"></i>
-							<span>
-                  Call +01 1234567890
-                </span>
+							<span>Call +01 1234567890</span>
 						</a>
 						<a href="">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
-							<span>
-                  demo@gmail.com
-                </span>
+							<span>demo@gmail.com</span>
 						</a>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-4 footer-col">
 				<div class="footer_detail">
-					<a href="" class="footer-logo">
-						Feane
-					</a>
-					<p>
-						Necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin
-						words, combined with
-					</p>
+					<a href="" class="footer-logo"><?= TITLE ?></a>
+					<p>Necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin
+						words, combined with</p>
 					<div class="footer_social">
 						<a href="">
 							<i class="fa fa-facebook" aria-hidden="true"></i>
@@ -58,25 +51,17 @@
 				</div>
 			</div>
 			<div class="col-md-4 footer-col">
-				<h4>
-					Opening Hours
-				</h4>
-				<p>
-					Everyday
-				</p>
-				<p>
-					10.00 Am -10.00 Pm
-				</p>
+				<h4>Opening Hours</h4>
+				<p>Everyday</p>
+				<p>10.00 Am -10.00 Pm</p>
 			</div>
 		</div>
 		<div class="footer-info">
 			<p>
 				&copy; <span id="displayYear"></span> All Rights Reserved By
-				<a href="https://html.design/">Free Html Templates</a><br><br>
-				&copy; <span id="displayYear"></span> Distributed By
-				<a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+				<a target="_blank" href="<?= SC_WWW ?>"><?= SC_NAME ?></a><br><br>
+				&copy; <span id="displayYear"></span> <?= $version ?>
 			</p>
 		</div>
 	</div>
 </footer>
-<!-- footer section -->
--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <html>
 <?php include "components/head.php"; ?>
 <body>
-<?php include "components/hero.php"; ?>
+<div class="hero_area">
+	<div class="bg-box">
+		<img src="images/hero-bg.jpg" alt="">
+	</div>
+	<?php include "components/header.php"; ?>
+	<?php include "components/slider.php"; ?>
+</div>
 <?php //include "components/todo.php"; ?>
-<?php //include "components/footer.php"; ?>
+<?php include "components/footer.php"; ?>
 <?php include "components/scripts.php"; ?>
 </body>
 </html>
