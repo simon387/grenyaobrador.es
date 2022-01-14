c3f0280416163778fe39b55de65eaf13a02faa38
simone <simon387@hotmail.it>
2022-01-15 00:37:22 +0100

    other_test

--- almacen/fe/components/sidebar.php
+++ almacen/fe/components/sidebar.php
@@ -1,7 +1,10 @@
+<?php
+$bs = ( in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) ? "localhost/" : "";
+?>
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar" style="display: none">
 
 	<!-- Sidebar - Brand -->
-	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="/grenyaobrador.es/almacen/fe/index.php">
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="//<?= $bs ?>grenyaobrador.es/almacen/fe/index.php">
 		<div class="sidebar-brand-icon rotate-n-15">
 			<em class="fas fa-laugh-wink"></em>
 		</div>
@@ -13,7 +16,7 @@
 
 	<!-- Nav Item - Dashboard -->
 	<li class="nav-item">
-		<a class="nav-link" href="/grenyaobrador.es/almacen/fe/index.php">
+		<a class="nav-link" href="//grenyaobrador.es/almacen/fe/index.php">
 			<em class="fas fa-fw fa-tachometer-alt"></em>
 			<span>Dashboard</span></a>
 	</li>
@@ -27,7 +30,7 @@
 	</div>
 
 	<li class="nav-item">
-		<a class="nav-link" href="/grenyaobrador.es/almacen/fe/components/pages/logs.php">
+		<a class="nav-link" href="//grenyaobrador.es/almacen/fe/components/pages/logs.php">
 			<em class="fas fa-fw fa-chart-area"></em>
 			<span>Operaciones</span></a>
 	</li>
@@ -49,9 +52,9 @@
 		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
 			<div class="bg-white py-2 collapse-inner rounded">
 				<h6 class="collapse-header">Personalizar:</h6>
-				<a class="collapse-item" href="/almacen/fe/components/pages/periods.php">Períodos</a>
-				<a class="collapse-item" href="/almacen/fe/components/pages/suppliers.php">Provedores</a>
-				<a onclick="return false;" class="collapse-item" href="/almacen/fe/components/pages/products.php">Productos</a>
+				<a class="collapse-item" href="//grenyaobrador.es/almacen/fe/components/pages/periods.php">Períodos</a>
+				<a class="collapse-item" href="//grenyaobrador.es//almacen/fe/components/pages/suppliers.php">Provedores</a>
+				<a onclick="return false;" class="collapse-item" href="//grenyaobrador.es//almacen/fe/components/pages/products.php">Productos</a>
 			</div>
 		</div>
 	</li>
