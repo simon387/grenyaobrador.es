749c34f0ebea04bb41a6f7a23386be0570b3cc61
simone <simon387@hotmail.it>
2022-01-14 04:25:34 +0100

    primi_conf_db

---
+++ dumps/create.sql
@@ -0,0 +1,14 @@
+-- create database
+create schema grenyaobrador;
+
+-- create roles
+create table role_
+(
+    id   int auto_increment
+        primary key,
+    name varchar(255) not null,
+    constraint role__name_uindex unique (name)
+);
+-- insert default roles
+INSERT INTO role_ (name, id) VALUES ('admin', 1);
+
