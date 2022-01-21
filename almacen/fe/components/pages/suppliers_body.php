214d557cae71648617f83d84d122a30e03d30a7f
simone <simon387@hotmail.it>
2022-01-21 10:21:10 +0100

    1.0.21

--- almacen/fe/components/pages/suppliers_body.php
+++ almacen/fe/components/pages/suppliers_body.php
@@ -1,9 +1,7 @@
 <div class="container-fluid">
 
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-		<h1 class="h3 mb-0 text-gray-800">
-			Lista de todos los Provedores de el almacén
-		</h1>
+		<h1 class="h3 mb-0 text-gray-800">Lista de todos los Proveedores de el almacén</h1>
 		<button class="btn btn-primary btn-icon-split btn-sm" id="btn-new-supplier">
 				<span class="icon text-white-50">
 					<em class="fas fa-plus"></em>
@@ -14,7 +12,7 @@
 
 	<div class="card shadow mb-4">
 		<div class="card-header py-3">
-			<h6 class="m-0 font-weight-bold text-primary">Provedores</h6>
+			<h6 class="m-0 font-weight-bold text-primary">Proveedores</h6>
 		</div>
 		<div class="card-body">
 			<div class="table-responsive overflow-hidden">
