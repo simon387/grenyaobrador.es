8b7db9ecb7ea2342f7be32223e04e6d0b6b25190
simone.celia <simone.celia@eng.it>
2022-10-22 03:29:01 +0200

    1.0.29

--- almacen/fe/components/footer.php
+++ almacen/fe/components/footer.php
@@ -9,7 +9,7 @@ fclose($f);
 <footer class="sticky-footer bg-white">
 	<div class="container my-auto">
 		<div class="copyright text-center my-auto">
-			<span>ALMACEN Grenyaobrador.es <?php echo $version ?> - Copyright &copy; <a target="_blank" href="https:\\www.simonecelia.it">www.simonecelia.it</a> 2022</span>
+			<span>ALMACEN Grenyaobrador.es <?php echo $version ?> - Copyright &copy; <a target="_blank" href="https://www.simonecelia.it">www.simonecelia.it</a> 2022</span>
 		</div>
 	</div>
 </footer>
